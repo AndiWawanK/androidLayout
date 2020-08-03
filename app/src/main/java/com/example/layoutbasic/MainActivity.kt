fb27e3a11a6114b2e58a1ece395afe0a3d2672da
AndiWawanK <andifirwansyah250@gmail.com>
2020-08-03 23:48:37 +0700

    simple profile layout

--- app/src/main/java/com/example/layoutbasic/MainActivity.kt
+++ app/src/main/java/com/example/layoutbasic/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.layoutbasic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.res.ResourcesCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var followText: Boolean = false
+        button8.setOnClickListener {
+            followText = !followText
+            if(followText == true){
+                val color = ResourcesCompat.getColor(getResources(), R.color.btnPrimaryDark, null)
+                button8.setText("Unfollow")
+                button8.setBackgroundColor(color)
+            }else{
+                val color = ResourcesCompat.getColor(getResources(), R.color.btnPrimary, null)
+                button8.setText("Follow")
+                button8.setBackgroundColor(color)
+            }
+        }
+
     }
 }
